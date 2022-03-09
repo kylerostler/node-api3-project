aa47e8c763f7dcb71c51f037e452a1d97d3ab2ca
kylerostler <93017344+kylerostler@users.noreply.github.com>
2022-03-09 14:57:39 -0700

    working on middleware

--- api/server.js
+++ api/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { logger, validatePost, validateUser, validateUserId } = require('./middleware/middleware')
 const server = express();
-
+const usersRouter = require('./users/users-router')
 server.use(express.json())
 
 server.use(logger)
-// global middlewares and the user's router need to be connected here
+
+server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
